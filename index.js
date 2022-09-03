cee59dce4d303a202061195b5975a598ad587cdc
Pixelsuft <pixelsuft@gmail.com>
2022-09-03 20:53:04 +0700

    avoid eval

--- index.js
+++ index.js
@@ -35,17 +35,17 @@ exports.load_sdl2_library = function(library_path, ...ignore_list) {
   api.ignore_list = ignore_list;
   api.library = api.ffi.Library(library_path, api.join_exports());
   Object.entries(api.join_functions()).forEach(([key, value]) => {
-    eval('api.library.' + key + ' = value');
+    api.library[key] = value;
   });
   exports.export_sdl2_library(api.l);
   return api.library;
 }
 exports.export_sdl2_library = function(export_obj) {
   Object.entries(api.join_exports()).forEach(([key, value]) => {
-    eval('export_obj.' + key + ' = api.library.' + key);
+    export_obj[key] = api.library[key];
   });
   Object.entries(api.join_functions()).forEach(([key, value]) => {
-    eval('export_obj.' + key + ' = value');
+    export_obj[key] = value;
   });
   return export_obj;
 }
@@ -56,7 +56,7 @@ exports.load_sdl2_image_library = function(library_path, ...ignore_list) {
 }
 exports.export_sdl2_image_library = function(export_obj) {
   Object.entries(require('./image').library_exports).forEach(([key, value]) => {
-    eval('export_obj.' + key + ' = api.image_library.' + key);
+    export_obj[key] = api.image_library[key];
   });
   return export_obj;
 }
@@ -67,7 +67,7 @@ exports.load_sdl2_ttf_library = function(library_path, ...ignore_list) {
 }
 exports.export_sdl2_ttf_library = function(export_obj) {
   Object.entries(require('./ttf').library_exports).forEach(([key, value]) => {
-    eval('export_obj.' + key + ' = api.ttf_library.' + key);
+    export_obj[key] = api.ttf_library[key];
   });
   return export_obj;
 }
@@ -78,7 +78,7 @@ exports.load_sdl2_mixer_library = function(library_path, ...ignore_list) {
 }
 exports.export_sdl2_mixer_library = function(export_obj) {
   Object.entries(require('./mixer').library_exports).forEach(([key, value]) => {
-    eval('export_obj.' + key + ' = api.mixer_library.' + key);
+    export_obj[key] = api.mixer_library[key];
   });
   return export_obj;
 }
@@ -86,16 +86,16 @@ exports.export_sdl2_mixer_library = function(export_obj) {
 exports.load_sdl2_net_library = function(library_path, ...ignore_list) {
   api.net_library = api.ffi.Library(library_path, api.clear_ignore(require('./net').library_exports, ignore_list));
   Object.entries(require('./net').library_functions).forEach(([key, value]) => {
-    eval('api.net_library.' + key + ' = value');
+    api.net_library[key] = value;
   });
   return api.net_library;
 }
 exports.export_sdl2_net_library = function(export_obj) {
   Object.entries(require('./net').library_exports).forEach(([key, value]) => {
-    eval('export_obj.' + key + ' = api.net_library.' + key);
+    export_obj[key] = api.net_library[key];
   });
   Object.entries(require('./net').library_functions).forEach(([key, value]) => {
-    eval('export_obj.' + key + ' = value');
+    export_obj[key] = value;
   });
   return export_obj;
 }
@@ -106,7 +106,7 @@ exports.load_sdl2_gfx_library = function(library_path, ...ignore_list) {
 }
 exports.export_sdl2_gfx_library = function(export_obj) {
   Object.entries(require('./gfx').library_exports).forEach(([key, value]) => {
-    eval('export_obj.' + key + ' = api.gfx_library.' + key);
+    export_obj[key] = api.gfx_library[key];
   });
   return export_obj;
 }
@@ -117,7 +117,7 @@ exports.load_sdl2_sound_library = function(library_path, ...ignore_list) {
 }
 exports.export_sdl2_sound_library = function(export_obj) {
   Object.entries(require('./sound').library_exports).forEach(([key, value]) => {
-    eval('export_obj.' + key + ' = api.sound_library.' + key);
+    export_obj[key] = api.sound_library[key];
   });
   return export_obj;
 }
@@ -128,7 +128,7 @@ exports.load_sdl2_rtf_library = function(library_path, ...ignore_list) {
 }
 exports.export_sdl2_rtf_library = function(export_obj) {
   Object.entries(require('./rtf').library_exports).forEach(([key, value]) => {
-    eval('export_obj.' + key + ' = api.rtf_library.' + key);
+    export_obj[key] = api.rtf_library[key];
   });
   return export_obj;
 }
